db590a5f037e4a320c5f3857dd2f56219835b9f1
inbargold <156782070+ofirperry2007@users.noreply.github.com>
2025-02-11 15:39:56 +0200

    Update ev3lego.h

--- ev3lego.h
+++ ev3lego.h
@@ -3,14 +3,14 @@
  class ev3lego {
 
   public:
-   ev3lego(int encoder1, int encoder2, int in1R, int in2R, int enaR, int in1L, int in2L, int enaL, int wheel);
+   ev3lego(int encoder1, int encoder2, int in10, int in20, int ena0, int wheel, int in11, int in21, int ena1); // R = 0 , L = 1 , W = 2
    void begin();   //must be called from setup()
    void run();   //must be called from loop()
    long ang();
    void godegrees(int angle, int times);
    void godegreesp(int angle, int times, int kp, int ki, int kd);
    double PIDcalc(double inp, int sp, int kp, int ki, int kd);
-   void motgo(int speed, char motnum);
+   void motgo(int speed, int motnum);
    double gomm(int distance, int times);
    double gommp(int distance, int times, int kp, int ki, int kd);
    int getYAW(); 
@@ -18,7 +18,7 @@
 
 
   private:
-   int _encoder1, _encoder2, _in1R, _in2R, _enaR, _in1L, _in2L, _enaL, _wheel;
+   int _encoder1, _encoder2, _in10, _in20, _ena0, _wheel, _in11, _in21, _ena1;
    double _kp, _ki, _kd;
    bool integralflag = false;
  };
